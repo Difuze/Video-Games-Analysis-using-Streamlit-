801f4433e204d1639e6e1de907a3effb75749c31
Harsh Jaiswal <110847443+Difuze@users.noreply.github.com>
2024-06-01 13:57:48 +0530

    Add files via upload

---
+++ games.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+
+
+st.set_page_config(
+    page_title="Video Games Analysis",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
